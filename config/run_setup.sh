fd75d76e21544c66890627da9ecf7028d70331a1
独沐晨霖 <1204572840@qq.com>
2023-12-17 17:49:27 +0800

    Update run_setup.sh

--- config/run_setup.sh
+++ config/run_setup.sh
@@ -4,11 +4,9 @@ export MYSQL_ROOT_PASS="root"
 export MYSQL_USER="root"
 export MYSQL_PASS="root"
 
-sudo mysql -e "create database dvwa;"
 sudo mysql -e "create user dvwa@localhost identified by 'p@ssw0rd';"
-sudo mysql -e "grant all on dvwa.* to dvwa@localhost;"
+sudo mysql -e "grant all on *.* to 'dvwa'@'localhost';"
 sudo mysql -e "flush privileges;"
-sudo mysql -e "drop database dvwa;"
 
 curl -X POST -c cookies.txt -d 'username=admin&password=password' http://localhost/login.php -L -o login.body &&\
 curl -b cookies.txt -X POST -d 'create_db=Create+%2F+Reset+Database' http://localhost/setup.php -L -o setup.body

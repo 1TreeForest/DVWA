da6bbc8f34995a1db4fa28f1996138bbae632a8f
独沐晨霖 <1204572840@qq.com>
2023-12-17 15:29:12 +0800

    Create run_setup.sh

---
+++ config/run_setup.sh
@@ -0,0 +1,10 @@
+export MYSQL_HOST="127.0.0.1"
+export MYSQL_ROOT_USER="root"
+export MYSQL_ROOT_PASS="root"
+export MYSQL_USER="root"
+export MYSQL_PASS="root"
+
+mysql -e "create database dvwa;"
+mysql -e "create user dvwa@localhost identified by 'p@ssw0rd';"
+mysql -e "grant all on dvwa.* to dvwa@localhost;"
+mysql -e "flush privileges;"
